d3e226a3393c2859e57b9422f64bc96e45782c78
zaksignu <zaksignu@gmail.com>
2022-01-27 11:51:35 +0300

    files and tests added

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+    }
+}
